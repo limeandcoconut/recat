f6decdab1ac9b2eb3e87016a484c6daa40cf7564
Jacob Smith <messagethesmith@gmail.com>
2019-03-25 00:08:52 -0700

    finish spinner

--- src/shared/components/beater/index.js
+++ src/shared/components/beater/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import styles from './beater.module.less'
 
-const Beater = ({display = true}) => {
+const Beater = ({display = true, className = ''}) => {
     if (!display) {
         return null
     }
-    return <div className={styles.beater}></div>
+
+    return (
+        <div className={`${styles.wrapper} ${className}`} >
+            <div className={styles.beater}></div>
+        </div>
+    )
 }
 
 export default Beater
