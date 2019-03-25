794bd7891a40cac90079d8c7407e7a3e2cbdb46f
Jacob Smith <messagethesmith@gmail.com>
2019-03-25 10:29:19 -0700

    add eslintrc to project

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+    extends: 'brian',
+    parser: 'babel-eslint',
+    plugins: [
+        'html',
+        'vue',
+    ],
+    rules: {
+        'indent': [2, 4, {MemberExpression: 0}],
+        'operator-linebreak': ['error', 'after', {overrides: {'?': 'before', ':': 'before'}}],
+    },
+}
