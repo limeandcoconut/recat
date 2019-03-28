152c18aec73ebf960eb61717caa1e038d7753db1
Jacob Smith <messagethesmith@gmail.com>
2019-03-28 00:16:48 -0700

    add eject directory with tools and samples

---
+++ eject/keys.sample.js
@@ -0,0 +1,12 @@
+// This file should be named 'keys.js' and placed in the project root
+const dbUsername = 'useruser'
+const dbPassword = 'passpasspass'
+const dbName = 'db'
+const encryptionKey = 'aEXACTLYthirtyTWOcharacterSTRING'
+
+module.exports = {
+    dbUsername,
+    dbPassword,
+    dbName,
+    encryptionKey,
+}
