ed1fe3837c5ccc822bee7a3e77f93ab51fdee188
Jacob Smith <messagethesmith@gmail.com>
2019-03-31 15:09:27 -0700

    clean up webpack client.prod.js a touch

--- config/webpack.config.js/client.prod.js
+++ config/webpack.config.js/client.prod.js
@@ -18,15 +18,12 @@ const config = {
             asset: '[path].br[query]',
             test: /\.js$|\.css$/,
             threshold: 0,
-            // minRatio: 0.8,
         }),
         // It'd be best to read options for this and cater to specific project needs
         // https://www.npmjs.com/package/sw-precache-webpack-plugin
         new SWPrecacheWebpackPlugin({
             filename: 'service-worker.js',
-            // staticFileGlobs: ['dist/**/*.{js,html,css}'],
-            // minify: true,
-            // stripPrefix: 'dist/',
+            minify: true,
             runtimeCaching: [{
                 urlPattern: '/*',
                 handler: 'networkFirst',
@@ -35,13 +32,11 @@ const config = {
                 // fastest
                 // networkOnly
                 // cacheOnly
-                // Why u no slowest()?
+                // WHY U NO slowest()?
             }],
             staticFileGlobs: [
                 path.join(paths.publicPath, '/**.css'),
-                // path.join(paths.publicPath, '/img/**.*'),
                 path.join(paths.publicPath, '/**.js'),
-                // path.join(paths.publicAssetPath, '/**.woff*'),
                 path.join(paths.publicAssetPath, '/**'),
             ],
             // Don't allow the service worker to try to cache google analytics or your tracking will stop working
