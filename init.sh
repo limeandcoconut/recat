129f6b7cd6c97403c64ff18b53de56ab05c0651f
Jacob Smith <messagethesmith@gmail.com>
2019-04-11 10:14:08 -0700

    fix init.sh for ubuntu

--- init.sh
+++ init.sh
@@ -47,9 +47,9 @@ cleancommand='rm '
 # Write files and concat command
 for i in 1 2 3 4 5
 do
-    base64 --decode -o ./images/raw/$i.png <<< iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=
-    base64 --decode -o ./images/webp/$i.webp <<< UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vv9UAA= 
-    base64 --decode -o ./images/brotli/$i.webp.br <<< cixWAUklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vv9UAAD 
+    echo iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII= |  base64 --decode > ./images/raw/$i.png
+    echo UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vv9UAA= |  base64 --decode > ./images/webp/$i.png.webp
+    echo cixWAUklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vv9UAAD |  base64 --decode > ./images/brotli/$i.png.webp.br
     cleancommand="$cleancommand $PWD/images/raw/$i.png $PWD/images/webp/$i.webp $PWD/images/brotli/$i.webp.br"
 done
 
